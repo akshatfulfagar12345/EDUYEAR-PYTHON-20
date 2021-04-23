4445357586d8e59f89e612063e3ac8aff7a6a233
akshatfulfagar12345 <83079062+akshatfulfagar12345@users.noreply.github.com>
2021-04-23 20:04:57 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,13 @@
+line='''to differentiate between married and unmarried men
+following names are given'''
+print(line)
+
+name1 = 'Swapnil'
+age1 = 31
+status1 = 'married'
+print('Member1:',name1,age1,status1)
+
+name2 = 'Pranav'
+age2 = 28
+status2 = 'unmarried'
+print('Member2:',name2,age2,status2)
