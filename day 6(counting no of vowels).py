e2ef92d6cbce76eec89f6b61287f053a4fd3cad0
akshatfulfagar12345 <83079062+akshatfulfagar12345@users.noreply.github.com>
2021-04-27 11:27:38 +0530

    Add files via upload

---
+++ day 6(counting no of vowels).py
@@ -0,0 +1,9 @@
+sentence = input('Enter the sentence:')
+string = sentence.lower()
+print(string)
+count = 0
+list1 = ['a','e','i','o','u']
+for char in string:
+    if char in list1:
+        count = count+1
+print('number of vowels in given sentence:',count)
