b528a6f5ad0062c49b4779e8604882a6849f4191
akshatfulfagar12345 <83079062+akshatfulfagar12345@users.noreply.github.com>
2021-04-30 13:16:31 +0530

    Add files via upload

---
+++ day 8( To check whether it is prime or not).py
@@ -0,0 +1,9 @@
+num = int(input('enter any positive number to check whether it is prime or not'))
+if num>1:
+    for i in range(2,num):
+        if (num%i)==0:
+            print(num,'is not a prime number')
+    else:
+        print(num,'is a prime number')
+else:
+    print(num,'is not a prime number')
